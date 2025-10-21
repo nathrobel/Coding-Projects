9ec2153de0c5d0668f7d483b170ce8983f12612a
Nathan Robel <nathanrobel6@gmail.com>
2025-10-21 14:11:25 +0100

    respond to echo

--- app/main.py
+++ app/main.py
@@ -6,9 +6,15 @@ def handle_request(client_socket):
     requested = data.decode()
     if requested.startswith("GET / HTTP/1.1"):
         response  = "HTTP/1.1 200 OK\r\n\r\n"
+    elif requested.startswith("GET /echo/"):
+            path = requested.split()[1]
+            message = path[len("/echo/"):]
+            content_length = len(message)
+            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {content_length}\r\n\r\n{message}"
     else:
         response  = "HTTP/1.1 404 Not Found\r\n\r\n"
     client_socket.sendall(response.encode())
+
 
 
 
