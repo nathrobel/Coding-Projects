467620c84ed89001c99fef1d8fe85c6baae207e0
Nathan Robel <nathanrobel6@gmail.com>
2025-10-22 17:00:45 +0100

    user-agent val

--- app/main.py
+++ app/main.py
@@ -7,10 +7,23 @@ def handle_request(client_socket):
     if requested.startswith("GET / HTTP/1.1"):
         response  = "HTTP/1.1 200 OK\r\n\r\n"
     elif requested.startswith("GET /echo/"):
-            path = requested.split()[1]
-            message = path[len("/echo/"):]
-            content_length = len(message)
-            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {content_length}\r\n\r\n{message}"
+        path = requested.split()[1]
+        message = path[len("/echo/"):]
+        content_length = len(message)
+        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {content_length}\r\n\r\n{message}"
+    elif requested.startswith("GET /user-agent"):
+        lines  = requested.splitlines()
+        user_agent_val = ""
+        for line in lines:
+            if line.lower().startswith("user-agent"):
+                user_agent_val = line[11:].strip()
+                break
+            else:
+                continue
+
+        user_agent_val_length = len(user_agent_val)
+        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {user_agent_val_length}\r\n\r\n{user_agent_val}"
+
     else:
         response  = "HTTP/1.1 404 Not Found\r\n\r\n"
     client_socket.sendall(response.encode())
