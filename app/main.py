8c08f162c3bbb59806ed208744a656767f4439fb
Nathan Robel <nathanrobel6@gmail.com>
2025-11-06 12:20:40 +0000

    persistent connection

--- app/main.py
+++ app/main.py
@@ -9,102 +9,110 @@ def handle_request(client_socket):
     #globa
     global directory
     try:
-        data = client_socket.recv(1024)
-        requested = data.decode()
-        if requested.startswith("GET / HTTP/1.1"):
-            response  = "HTTP/1.1 200 OK\r\n\r\n"
-        elif requested.startswith("GET /echo/"):
-            path = requested.split()[1]
-            message = path[len("/echo/"):]
-            content_length = len(message)
-            lines = requested.splitlines()
-            header_lines = lines[1:]
-
-            
-            accept_encodings = []
-            for line in header_lines:
-                if line.lower().startswith("accept-encoding:"):
-                    portion = line[len("accept-encoding:"):].strip()
-                    accept_encodings = [e.strip() for e in portion.split(",")]
-                    break
-            
-            compressed_body = gzip.compress(message.encode())
-
-            if "gzip" in accept_encodings:
-                headers = (
-                    "HTTP/1.1 200 OK\r\n"
-                    "Content-Encoding: gzip\r\n"
-                    "Content-Type: text/plain\r\n"
-                    f"Content-Length: {len(compressed_body)}\r\n"
-                    "\r\n"
-                ).encode()
-                response = headers + compressed_body
-                client_socket.sendall(response)
-                return 
-                
-            else:
-                response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {content_length}\r\n\r\n{message}"
+        while True:
+            data = client_socket.recv(1024)
+            if not data:
+                break
+            requested = data.decode()
+            keep_alive = True  
+            if "Connection: close" in requested.lower():
+                keep_alive = False
+            if requested.startswith("GET / HTTP/1.1"):
+                response  = "HTTP/1.1 200 OK\r\n\r\n"
+            elif requested.startswith("GET /echo/"):
+                path = requested.split()[1]
+                message = path[len("/echo/"):]
+                content_length = len(message)
+                lines = requested.splitlines()
+                header_lines = lines[1:]
 
                 
+                accept_encodings = []
+                for line in header_lines:
+                    if line.lower().startswith("accept-encoding:"):
+                        portion = line[len("accept-encoding:"):].strip()
+                        accept_encodings = [e.strip() for e in portion.split(",")]
+                        break
+                
+                compressed_body = gzip.compress(message.encode())
 
-
-        elif requested.startswith("GET /user-agent"):
-            lines  = requested.splitlines()
-            user_agent_val = ""
-            for line in lines:
-                if line.lower().startswith("user-agent:"):
-                    user_agent_val = line[11:].strip()
-                    break
+                if "gzip" in accept_encodings:
+                    headers = (
+                        "HTTP/1.1 200 OK\r\n"
+                        "Content-Encoding: gzip\r\n"
+                        "Content-Type: text/plain\r\n"
+                        f"Content-Length: {len(compressed_body)}\r\n"
+                        "\r\n"
+                    ).encode()
+                    response = headers + compressed_body
+                    client_socket.sendall(response)
+                    continue 
+                    
                 else:
+                    response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {content_length}\r\n\r\n{message}"
+
+                    
+
+
+            elif requested.startswith("GET /user-agent"):
+                lines  = requested.splitlines()
+                user_agent_val = ""
+                for line in lines:
+                    if line.lower().startswith("user-agent:"):
+                        user_agent_val = line[11:].strip()
+                        break
+                    else:
+                        continue
+
+                user_agent_val_length = len(user_agent_val)
+                response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {user_agent_val_length}\r\n\r\n{user_agent_val}"
+
+
+
+
+            elif requested.startswith("GET /files/"):
+                path = requested.split()[1]
+                filename = path[len("/files/"):]
+                filepath = f"{directory}/{filename}"
+                try:
+                    with open(filepath,"rb") as f:
+                        content = f.read()
+                        length = len(content)
+                        headers = (f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length:{length} \r\n\r\n")
+                        client_socket.sendall(headers.encode() + content)
+                        continue
+                        
+                except FileNotFoundError:
+                    response = "HTTP/1.1 404 Not Found\r\n\r\n"
+                    client_socket.sendall(response.encode())
+                    continue 
+            elif requested.startswith("POST /files/"):
+                path = requested.split()[1]
+                filename = path[len("/files/"):]
+                filepath = f"{directory}/{filename}"
+                header_body = requested.split("\r\n\r\n")
+                
+
+                try:
+                    with open(filepath,"wb") as f:
+                        content = header_body[1]
+                        f.write(content.encode())
+                        response = (f"HTTP/1.1 201 Created\r\n\r\n")
+                        client_socket.sendall(response.encode())
+                        continue
+                except Exception as e:
+                    print(f"error writing file:{e}")
+                    response = "HTTP/1.1 500 Internal Server Error\r\n\r\n"
+                    client_socket.sendall(response.encode())
                     continue
 
-            user_agent_val_length = len(user_agent_val)
-            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {user_agent_val_length}\r\n\r\n{user_agent_val}"
 
 
-
-
-        elif requested.startswith("GET /files/"):
-            path = requested.split()[1]
-            filename = path[len("/files/"):]
-            filepath = f"{directory}/{filename}"
-            try:
-                with open(filepath,"rb") as f:
-                    content = f.read()
-                    length = len(content)
-                    headers = (f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length:{length} \r\n\r\n")
-                    client_socket.sendall(headers.encode() + content)
-                    return
-                    
-            except FileNotFoundError:
-                response = "HTTP/1.1 404 Not Found\r\n\r\n"
-                client_socket.sendall(response.encode())
-                return 
-        elif requested.startswith("POST /files/"):
-            path = requested.split()[1]
-            filename = path[len("/files/"):]
-            filepath = f"{directory}/{filename}"
-            header_body = requested.split("\r\n\r\n")
-            
-
-            try:
-                with open(filepath,"wb") as f:
-                    content = header_body[1]
-                    f.write(content.encode())
-                    response = (f"HTTP/1.1 201 Created\r\n\r\n")
-                    client_socket.sendall(response.encode())
-                    return
-            except Exception as e:
-                print(f"error writing file:{e}")
-                response = "HTTP/1.1 500 Internal Server Error\r\n\r\n"
-                client_socket.sendall(response.encode())
-                return
-
-
-
-        else:
-            response  = "HTTP/1.1 404 Not Found\r\n\r\n"
-        client_socket.sendall(response.encode())
+            else:
+                response  = "HTTP/1.1 404 Not Found\r\n\r\n"
+            client_socket.sendall(response.encode())
+            if not keep_alive:
+                break
     finally:
         client_socket.close()
 
